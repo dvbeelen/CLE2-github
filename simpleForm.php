d1b5071e96b80fef3e43e9c4c348a8e061348052
dvanbeelen <>
2019-01-04 15:21:29 +0100

    Added new form
    Added in a new form that shows all inputfields on one screen.

---
+++ simpleForm.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="icon" type="ïmage/jpg" href="images/smalllogo.jpg"/>
+    <link href="https://fonts.googleapis.com/css?family=Bubbler+One" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Maak een afspraak</title>
+</head>
+<body>
+<header>
+    <div class="logo">
+        <img src="images/logo.jpg">
+    </div>
+    <nav>
+        <button class="navButton"><a href="https://www.didypedicure.nl/">Home</a></button>
+        <button class="navButton"><a href="https://www.didypedicure.nl/aantekeningen/">Aantekeningen</a></button>
+        <button class="navButton"><a href="https://www.didypedicure.nl/prijslijst/">Prijslijst</a></button>
+        <button class="navButton"><a href="">Afspraak Maken</a></button>
+        <button class="navButton"><a href="https://www.didypedicure.nl/de-praktijk/">De Praktijk</a></button>
+        <button class="navButton"><a href="https://www.didypedicure.nl/contact/">Contact</a></button>
+        <button class="navButton"><a href="https://www.didypedicure.nl/privacy/">Privacy</a></button>
+    </nav>
+</header>
+
+<div class= "appointFolder">
+    <form class="form">
+        <label for="firstname">Voornaam:</label> <br>
+        <input type="text" id="firstname"> <br>
+        <label for="lastname">Achternaam:</label> <br>
+        <input type="text" id="firstname"> <br>
+        <label for="email">E-mail:</label> <br>
+        <input type="email" id="email"> <br>
+        <label for="phonenumber">Telefoonnummer:</label> <br>
+        <input type="number" id="phonenumber"> <br>
+        <label for="date">Kies een datum</label> <br>
+        <input type="date" id="date"> <br>
+        <label for="time">Kies een tijd:</label> <br>
+        <input type="number" id="time"> <br> <br>
+        <button id="submit">Afspraak maken </button>
+</div>
+
